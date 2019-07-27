b02452a55f449b4d3e527d260fc161bd5e9d3481
Jumpei Ogawa <phanective@gmail.com>
2019-07-27 22:05:36 +0900

    test: add conditions

--- test/test.js
+++ test/test.js
@@ -3,13 +3,129 @@ import test from "ava";
 import sao from "sao";
 
 const generator = path.join(__dirname, "..");
+const conditions = [
+  {
+    name: "foo",
+    isPersonal: true,
+    lib: true,
+    typescript: true,
+    env: "browser",
+  },
+  {
+    name: "foo",
+    isPersonal: true,
+    lib: true,
+    typescript: true,
+    env: "node",
+  },
+  {
+    name: "foo",
+    isPersonal: true,
+    lib: true,
+    typescript: false,
+    env: "browser",
+  },
+  {
+    name: "foo",
+    isPersonal: true,
+    lib: true,
+    typescript: false,
+    env: "node",
+  },
+  {
+    name: "foo",
+    isPersonal: true,
+    lib: false,
+    typescript: true,
+    env: "browser",
+  },
+  {
+    name: "foo",
+    isPersonal: true,
+    lib: false,
+    typescript: true,
+    env: "node",
+  },
+  {
+    name: "foo",
+    isPersonal: true,
+    lib: false,
+    typescript: false,
+    env: "browser",
+  },
+  {
+    name: "foo",
+    isPersonal: true,
+    lib: false,
+    typescript: false,
+    env: "node",
+  },
+  {
+    name: "foo",
+    isPersonal: false,
+    lib: true,
+    typescript: true,
+    env: "browser",
+  },
+  {
+    name: "foo",
+    isPersonal: false,
+    lib: true,
+    typescript: true,
+    env: "node",
+  },
+  {
+    name: "foo",
+    isPersonal: false,
+    lib: true,
+    typescript: false,
+    env: "browser",
+  },
+  {
+    name: "foo",
+    isPersonal: false,
+    lib: true,
+    typescript: false,
+    env: "node",
+  },
+  {
+    name: "foo",
+    isPersonal: false,
+    lib: false,
+    typescript: true,
+    env: "browser",
+  },
+  {
+    name: "foo",
+    isPersonal: false,
+    lib: false,
+    typescript: true,
+    env: "node",
+  },
+  {
+    name: "foo",
+    isPersonal: false,
+    lib: false,
+    typescript: false,
+    env: "browser",
+  },
+  {
+    name: "foo",
+    isPersonal: false,
+    lib: false,
+    typescript: false,
+    env: "node",
+  },
+]
 
-test("defaults", async t => {
-  const stream = await sao.mock({ generator });
+for (const [ i, condition ] of Object.entries(conditions)) {
+  test(`Generate files - ${i}`, async t => {
+    const stream = await sao.mock({ generator }, condition);
 
-  t.snapshot(stream.fileList, "Generated files");
+    t.snapshot(stream.fileList, "Generated files");
 
-  for (const file of stream.fileList) {
-    t.snapshot(await stream.readFile(file), file);
-  }
-});
+    for (const file of stream.fileList) {
+      t.snapshot(await stream.readFile(file), file);
+    }
+  });
+}
