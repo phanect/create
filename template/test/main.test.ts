99d19e36284e6089781f6ca57b6bad4fc1619241
Jumpei Ogawa <phanective@gmail.com>
2020-01-26 00:23:17 +0900

    feat: add test templates

---
+++ template/test/main.test.ts
@@ -0,0 +1,19 @@
+import { Server } from "./testutils";
+
+const port = 3456;
+const url = `http://localhost:${port}`;
+
+let server: Server;
+
+beforeEach(async () => {
+  server = new Server(port);
+  await server.start();
+});
+
+afterEach(async () => {
+  await server.close();
+});
+
+test("test", async () => {
+  expect(1).toBe(1);
+}, 50000);
