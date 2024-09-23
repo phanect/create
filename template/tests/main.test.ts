03e33dff7d7eeacf4522e4aed75c38d33255a323
Jumpei Ogawa <git@phanective.org>
2024-09-23 13:29:43 +0900

    refactor: remove no longer used testutils

--- template/tests/main.test.ts
+++ template/tests/main.test.ts
@@ -1,18 +1,11 @@
 import { afterEach, beforeEach, expect, test } from "vitest";
-import { Server } from "./testutils";
 
-const port = 3456;
-const url = `http://localhost:${port}`;
-
-let server: Server;
-
-beforeEach(async () => {
-  server = new Server(port);
-  await server.start();
+beforeEach(() => {
+  // Write code
 });
 
-afterEach(async () => {
-  await server.close();
+afterEach(() => {
+  // Write code
 });
 
 test("test", async () => {
