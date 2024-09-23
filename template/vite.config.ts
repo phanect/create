61b9f11e7aa418148569675e64bf0fae239e428a
Jumpei Ogawa <git@phanective.org>
2024-09-23 12:58:38 +0900

    feat: use `import.meta.dirname`

--- template/vite.config.ts
+++ template/vite.config.ts
@@ -1,20 +1,17 @@
 /// <reference types="vitest" />
 import { readFile } from "node:fs/promises";
 import { join } from "node:path";
-import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
-
-const packageJson = JSON.parse((await readFile(join(__dirname, "package.json"))).toString());
+const packageJson = JSON.parse((await readFile(join(import.meta.dirname, "package.json"))).toString());
 const deps = Object.keys(packageJson.dependencies ?? {});
 
 export default defineConfig({
 <% if (type === "lib") { -%>
   build: {
     lib: {
-      entry: join(__dirname, "src/main.ts"),
+      entry: join(import.meta.dirname, "src/main.ts"),
       name: "main",
       fileName: "main",
       formats: [ "es", "cjs" ],
@@ -25,8 +22,8 @@ export default defineConfig({
   },
   plugins: [
     dts({
-      outDir: join(__dirname, "dist/types"),
-      entryRoot: join(__dirname, "src"),
+      outDir: join(import.meta.dirname, "dist/types"),
+      entryRoot: join(import.meta.dirname, "src"),
       rollupTypes: true, // Generate single d.ts file
     }),
   ],
